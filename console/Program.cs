aa7d18d39cca2ad44cf1e1a9b565b97a11bda5d4
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2025-06-19 19:55:43 -0400

    initial commit, working generator for ToStringGenerator

---
+++ console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using generator;
+
+namespace sourcegen {
+    class Program 
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+
+            var p = new Person {
+                Name = "John",
+                Age = 30,
+                Email = "john@example.com"
+            };
+
+            Console.WriteLine(p.ToString());
+        }
+    }
+
+    [GenerateToString]
+    public partial class Person{
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ tests/ToStringGenerator_Tests.cs
@@ -0,0 +1,25 @@
+﻿using generator;
+
+namespace tests;
+
+public class ToStringGenerator_Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        var p = new Person {
+            Name = "John",
+            Age = 30,
+            Email = "john@example.com"
+        };
+        
+        Assert.Equal(p.ToString(), "Generated ToString for Person");
+    }
+}
+
+[GenerateToString]
+public partial class Person{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+    public string? Email { get; set; }
+}
